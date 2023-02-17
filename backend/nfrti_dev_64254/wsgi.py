5128ac2036af8e28a432c197aee168436a67a10d
Crowdbotics <engineering@crowdbotics.com>
2023-02-17 06:05:32 +0000

    Initial commit.

---
+++ backend/nfrti_dev_64254/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nfrti_dev_64254 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nfrti_dev_64254.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nfrti-dev-64254",
+  url: "https://nfrti-dev-64254.botics.co",
+  api: "https://nfrti-dev-64254.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
